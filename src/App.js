e6980be0a9e50623a9b4f2fa7263c6d7aa6c9401
Naomi Carey <naomicarey00@gmail.com>
2021-05-25 13:45:55 +0200

    installed: redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => setCounter((addOne) => addOne + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCounter((minusOne) => minusOne - 1)}>
+        Decrement
+      </button>
     </div>
   );
 }
